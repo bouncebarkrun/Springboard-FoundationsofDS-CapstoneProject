cde8a7468983f8605b7963c0b26ca374f910f523
Molly McNamara <bouncebarkrun@gmail.com>
2017-06-28 16:34:59 -0600

    Add files via upload

--- R code files/Data Analysis.R
+++ R code files/Data Analysis.R
@@ -4,6 +4,9 @@ library(readr)
 library(ggplot2)
 library(tidyr)
 library(ranger)
+library(caret)
+library(e1071)
+library(caTools)
 
 #Import and refine dataset
 FullData <- read.csv("C:/Users/mmcnamara/Desktop/FinalData_clean.csv")
@@ -31,8 +34,36 @@ summary(test)
 #Logistic regression model
 Logmodel <- glm(Playoffs ~ Shooting_Hand + YearsExperience + Position_Played + Birth_Country + BirthRegion + Games_Played + Goals + Assists + Points + Penalty_Minutes + Plus_Minus + Shots + GoalsPerGame + ShotsPerGame + PointsPerGame + PercentGoals + PercentGames + Draft_Pick + Draft_Round + Draft_Age + AmateurTeam + AmateurLeague, family="binomial", train)
 
+Logmodel
 # Predict on test: p
 p <- predict(Logmodel, test, type="response")
-summary(p)
 
-#Random forest model
+# Calculate class probabilities: p_class
+p_class <-
+  ifelse(p > 0.9,
+         "0",
+         "1"
+  )
+
+# Create confusion matrix
+confusionMatrix(p_class, test[["Playoffs"]])
+
+#Create a ROC curve
+colAUC(p, test[["Playoffs"]], plotROC = TRUE)
+
+as.factor(FullData$Playoffs)
+levels(FullData$Playoffs) <- c("0", "1")
+summary(FullData$Playoffs)
+str(FullData$Playoffs)
+
+# Fit random forest: model
+model <- train(
+  as.factor(Playoffs) ~ Shooting_Hand + YearsExperience + Position_Played + Birth_Country + BirthRegion + Games_Played + Goals + Assists + Points + Penalty_Minutes + Plus_Minus + Shots + GoalsPerGame + ShotsPerGame + PointsPerGame + PercentGoals + PercentGames + Draft_Pick + Draft_Round + Draft_Age + AmateurTeam + AmateurLeague,
+  tuneLength = 3,
+  data = FullData, method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, verboseIter = TRUE), na.action=na.omit
+)
+
+# Print model to console
+model 
+plot(model)
